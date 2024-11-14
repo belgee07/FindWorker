4a1b458e8afc3750103ec8d559a176ae6b4228bd
24LP2301 24LP2301 <24LP2301@G302-CC0089.local>
2024-11-14 11:38:49 +0800

    home zas

--- front-end/src/components/HomePage.tsx
+++ front-end/src/components/HomePage.tsx
@@ -3,23 +3,28 @@ import { Button } from "@/components/ui/button";
 import { FiUser } from "react-icons/fi";
 import { CgLogIn } from "react-icons/cg";
 import Link from "next/link";
+import { Workers } from "./Workers";
 
 export const HomePage = () => {
   return (
-    <div className="relative ">
-      <img src="./banner.png" alt="Banner" className="w-full h-auto" />
-
-      <div className="absolute bottom-64 left-32 flex space-x-4">
-        <Link href={"/sign-up"}>
-          <Button className="bg-black rounded-3xl">
-            <FiUser /> Бүртгүүлэх
-          </Button>
-        </Link>
-        <Link href={"/sign-in"}>
-          <Button className="bg-black rounded-3xl">
-            <CgLogIn /> Нэвтрэх
-          </Button>
-        </Link>
+    <div>
+      <div className="relative ">
+        <img src="./banner.png" alt="Banner" className="w-full h-auto" />
+        <div className="absolute bottom-64 left-32 flex space-x-4">
+          <Link href={"/sign-up"}>
+            <Button className="bg-black rounded-3xl">
+              <FiUser /> Бүртгүүлэх
+            </Button>
+          </Link>
+          <Link href={"/sign-in"}>
+            <Button className="bg-black rounded-3xl">
+              <CgLogIn /> Нэвтрэх
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Workers />
       </div>
     </div>
   );
