31bcd5038ab909e11bdcf720646ed00664e10b76
24LP6422 24LP6422 <24LP6422@G302-CC0088.local>
2024-11-19 12:20:57 +0800

    all worker data

--- back-end/src/routes/WorkerRoutes.ts
+++ back-end/src/routes/WorkerRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { registerWorker, updatedWorker, getWorkerWithDetails } from "../../controllers";
+import {
+  registerWorker,
+  updatedWorker,
+  getWorkerWithDetails,
+  getAllWorkers,
+} from "../../controllers";
 
 const router = Router();
 
 router.post("/register", registerWorker);
 router.put("/editWorker/:id", updatedWorker);
 router.get("/workerDetails/:id", getWorkerWithDetails);
+router.get("/allWorker", getAllWorkers);
 
 export default router;
