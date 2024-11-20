66814025a34b4ef87071bfe5fb070ecf7667b7a1
24LP6422 24LP6422 <24LP6422@G302-CC0088.local>
2024-11-20 12:21:29 +0800

    client data add

--- back-end/controllers/workerController/workerCreate.ts
+++ back-end/controllers/workerController/workerCreate.ts
@@ -7,8 +7,15 @@ export const registerWorker = async (
 ): Promise<void> => {
   const { authId, username, email } = req.body;
 
+  const actualEmail = typeof email === "object" ? email.emailAddress : email;
+
+  if (!actualEmail) {
+    res.status(400).json({ error: "Invalid email format" });
+    return;
+  }
+
   try {
-    const existingWorker = await WorkerModel.findOne({ email });
+    const existingWorker = await WorkerModel.findOne({ email: actualEmail });
 
     if (existingWorker) {
       res.status(409).json({ error: "Email already in use" });
@@ -18,11 +25,12 @@ export const registerWorker = async (
     const worker = new WorkerModel({
       authId,
       username,
-      email,
+      email: actualEmail,
     });
 
     await worker.save();
     res.status(201).json(worker);
+    console.log("Register successfully");
   } catch (error) {
     console.error("Error creating worker:", error);
     res.status(500).json({ error: "Worker creation failed" });
