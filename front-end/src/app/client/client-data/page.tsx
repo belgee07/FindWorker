7e06cdb40a51c9363dc0986ef294c1f57bf2417b
Tsas Tsagaanaa <tsagaanaa0210@gmail.com>
2024-11-21 12:39:21 +0800

    client

--- front-end/src/app/client/client-data/page.tsx
+++ front-end/src/app/client/client-data/page.tsx
@@ -1,8 +1,15 @@
 import { Category } from "@/components/Category";
+
+import { DataClient } from "@/components/DataClient";
 import React from "react";
 
 const ClientPage = () => {
-  return <div>Client-data</div>;
+  return (
+    <div>
+      {/* <ClientData /> */}
+      <DataClient />
+    </div>
+  );
 };
 
 export default ClientPage;
