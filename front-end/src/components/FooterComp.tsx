28a0903b2ba44a60d207b06c75a0d3c7f3041398
Zoloo <mac@macs-MacBook-Air.local>
2024-11-21 09:57:29 +0800

    responsive

--- front-end/src/components/FooterComp.tsx
+++ front-end/src/components/FooterComp.tsx
@@ -1,43 +1,49 @@
 import React from "react";
 import { FaPhone } from "react-icons/fa6";
 import { IoIosMail } from "react-icons/io";
+import { GrLocation } from "react-icons/gr";
+import { FaFacebook } from "react-icons/fa6";
 
 export const FooterComp = () => {
   return (
     <div className="h-fit bg-black text-white">
-      <div className="flex justify-between px-96 py-12">
-        <div className="flex gap-36">
-          <div className="flex flex-col gap-4">
-            <div>Ажлын байрууд</div>
-            <div className="flex flex-col gap-2 text-sm">
-              <div>Арт</div>
-              <div>Орчуулга</div>
-              <div>Гэр ахуй</div>
-              <div>Боловсрол</div>
-              <div>Гоо сайхан</div>
-              <div> Дизайн & Барилга</div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div>Байгууллага</div>
-            <div className="flex flex-col gap-2 text-sm">
-              <div>Үйл ажиллагаа</div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
+      <div className="flex justify-between  px-5 gap-8 py-10 md:px-14 lg:px-24 xl:px-44 2xl:px-96">
+        <div className="flex  flex-col gap-4">
+          <div className="font-bold">Ажлын байрууд</div>
+          <div className="flex flex-col gap-2 text-sm">
+            <div>Арт</div>
+            <div>Орчуулга</div>
+            <div>Гэр ахуй</div>
+            <div>Боловсрол</div>
+            <div>Гоо сайхан</div>
+            <div> Дизайн & Барилга</div>
           </div>
         </div>
-        <div className="flex gap-6">
+
+        <div className="flex flex-col gap-5">
           <div className="flex items-center gap-2">
-            <div className=" flex justify-center items-center w-8 h-8 border-gray-500 border-opacity-50 border-2 rounded-full ">
+            <div className="flex justify-center items-center w-10 h-10 border-gray-500 border-opacity-50 border-2 rounded-full hover:bg-sky-500 hover:border-orange-500 transition-all duration-300 ease-in-out ">
+              <GrLocation />
+            </div>
+
+            <p>Улаанбаатар, Монгол Улс</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className=" flex justify-center items-center w-10 h-10 border-gray-500 border-opacity-50 border-2 rounded-full hover:bg-blue-500 hover:border-orange-500  transition-all duration-300 ease-in-out">
+              <FaFacebook />
+            </div>
+
+            <p>Findwork</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className=" flex justify-center items-center w-10 h-10 border-gray-500 border-opacity-50 border-2 rounded-full  hover:bg-green-500 hover:border-pink-500 transition-all duration-300 ease-in-out ">
               <FaPhone />
             </div>
 
             <p>(976)80008000</p>
           </div>
           <div className="flex items-center gap-2">
-            <div className=" flex justify-center items-center w-8 h-8 border-gray-500 border-opacity-50 border-2 rounded-full ">
+            <div className=" flex justify-center items-center w-10 h-10 border-gray-500 border-opacity-50 border-2 rounded-full  hover:bg-indigo-500 hover:border-orange-500 transition-all duration-300 ease-in-out">
               <IoIosMail />
             </div>
 
@@ -49,7 +55,7 @@ export const FooterComp = () => {
         <p className="h-5 w-5 border-solid opacity-50  flex border-2 justify-center items-center  rounded-full">
           c
         </p>
-        <p>2024 Mon Service MN</p>
+        <p>2024 FindWork MN</p>
       </div>
     </div>
   );
--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { BiSearchAlt } from "react-icons/bi";
-import { useState } from "react";
+
 import { Button } from "@/components/ui/button";
 import { FiUser } from "react-icons/fi";
 import { CgLogIn } from "react-icons/cg";
+import { IoSearch } from "react-icons/io5";
+
+import { Input } from "@/components/ui/input";
+
 import {
   SignedIn,
   SignedOut,
@@ -34,24 +37,23 @@ export const Header = () => {
   }
 
   return (
-    <div className="flex border-b-2 items-center gap-32 justify-between px-96 py-3">
+    <div className="flex w-100% border-b-2 gap-2 px-5 justify-between  items-center md:px-14 lg:px-24 xl:px-44 2xl:px-96">
       <div>
         <Link href={"/"}>
-          <div className="text-3xl">
+          <div className="sm:text-3xl text-2xl ">
             <strong>Findwork</strong>
           </div>
         </Link>
 
-        <div>Монголын иргэн бүрд ажлын байр!</div>
+        <div className="text-xs sm:lg">Монголын иргэн бүрд ажлын байр!</div>
       </div>
-
-      <div className="flex items-center border border-gray-300 hover:border-gray-500 rounded-3xl px-4 py-2 w-80 gap-3">
-        <BiSearchAlt />
-        <input
-          type="text"
-          placeholder="Search..."
-          className="bg-transparent border-none outline-none text-gray-700 placeholder-gray-400 h-8"
+      <div className="relative ">
+        <Input
+          className=" pl-8 pr-2 py-1 rounded-2xl text-sm w-24  "
+          type="email"
+          placeholder="Хайх"
         />
+        <IoSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 text-xl" />
       </div>
 
       <div className="flex gap-2">
@@ -73,7 +75,7 @@ export const Header = () => {
           {/* Display the UserButton and username when the user is signed in */}
           {user && (
             <div className="flex items-center gap-2">
-              <span className="font-medium text-gray-700">
+              <span className="font-medium  text-gray-700">
                 Welcome, {user.username || "User"}!
               </span>
               <UserButton />
