70a4b832d691513ffef4909bb522b09ccf097c4d
24LP6422 24LP6422 <24LP6422@G302-CC0088.local>
2024-11-22 12:12:48 +0800

    asad

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    ACCESS_KEY: process.env.ACCESS_KEY,
+    SSECRET_ACCESS_KEY: process.env.SSECRET_ACCESS_KEY,
+  },
 };
 
 export default nextConfig;
