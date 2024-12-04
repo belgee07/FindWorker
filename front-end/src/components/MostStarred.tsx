3e91e5405277591b22cd61678a50aad391ae651c
24LP5438 24LP5438 <24LP5438@G301-CC-206.local>
2024-12-04 10:30:36 +0800

    ignore

---
+++ front-end/src/components/MostStarred.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export function MostStarred() {
+  return (
+    <div>
+      <div className="font-bold">Хамгийн өндөр үнэлгээтэй гүйцэтгэгчид</div>
+      <div></div>
+    </div>
+  );
+}
